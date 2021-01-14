996321cc71e0c6bda71576f8b82069761310b6f2
Fouad Chmainy <fchmainy@yahoo.com>
2021-01-14 14:11:57 +0100

    yes

--- install-controller.sh
+++ install-controller.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo apt install docker-ce
+sudo apt install -y docker-ce
 
 sudo apt update
 sudo apt install -y conntrack coreutils curl ebtables ethtool gettext grep gzip iproute2 iptables jq less openssl sed socat tar util-linux wget
 
 
+su -u $3 -s
+wget $1 -O /home/$3/controller.tar.gz
+cd /home/$3
+tar -xvf controller.tar.gz
+chown -R $3 controller-installer
+cd controller-installer/
 
 export CTR_SMTP_HOST='localhost'
 export CTR_SMTP_PORT='25'
@@ -26,8 +32,4 @@ export CTR_LASTNAME='Chmainy'
 export CTR_FQDN=$2
 export CTR_TSDB_VOL_TYPE='local'
 
-wget $1 -O controller.tar.gz
-tar -xvf controller.tar.gz
-cd controller-installer/
- 
 ./install.sh -y --self-signed-cert --non-interactive
