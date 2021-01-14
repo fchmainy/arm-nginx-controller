b28216c5fc5ac19fa651f952f8bc2eda63ddc5f9
Fouad Chmainy <fchmainy@yahoo.com>
2021-01-14 15:49:20 +0100

    yes

--- install-controller.sh
+++ install-controller.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
@@ -9,27 +8,18 @@ sudo apt install -y docker-ce
 sudo apt update
 sudo apt install -y conntrack coreutils curl ebtables ethtool gettext grep gzip iproute2 iptables jq less openssl sed socat tar util-linux wget
 
+whoami
+pwd
+echo $1
+echo $2
+echo $3
+echo $4
+echo $5
 
 wget $1 -O /home/$3/controller.tar.gz
 cd /home/$3
 tar -xvf controller.tar.gz
-chown -R $3 controller-installer
-su $3
-cd controller-installer/
+chown -R $3 /home/$3/controller-installer
 
-export CTR_SMTP_HOST='localhost'
-export CTR_SMTP_PORT='25'
-export CTR_SMTP_TLS=False
-export CTR_SMTP_AUTH=False
-export CTR_SMTP_USER=$3
-export CTR_SMTP_PASS=$4
-export CTR_SMTP_FROM='controller@f5demolab.org'
-export CTR_CONFIGDB_VOL_TYPE='local'
-export CTR_EMAIL=$5
-export CTR_PASSWORD=$4
-export CTR_FIRSTNAME='Fouad'
-export CTR_LASTNAME='Chmainy'
-export CTR_FQDN=$2
-export CTR_TSDB_VOL_TYPE='local'
-
-./install.sh -y --self-signed-cert --non-interactive
+echo 'now installing controller...'
+/bin/su -c "/home/$3/controller-installer/install.sh -y --self-signed-cert --non-interactive --tsdb-volume-type local -m localhost -x 25 -g false -b false -l $3 -q $4 -j controller@f5demolab.org -f $2 -t admin -u f5demo -e $5 -p $4" - $3
